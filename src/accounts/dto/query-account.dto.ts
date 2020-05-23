1b8e404dd8ec0728db102d06b6476d69c161bbe0
vspirit803 <lx546488173@gmail.com>
2020-05-23 19:17:56 +0800

    fix: fix bugs on querying accounts by name

--- src/accounts/dto/query-account.dto.ts
+++ src/accounts/dto/query-account.dto.ts
@@ -1,7 +1,9 @@
+import { Expose } from 'class-transformer';
 import { IsOptional, IsString } from 'class-validator';
 
 export class QueryAccountDto {
   @IsOptional()
+  @Expose()
   @IsString()
   readonly name: string;
 }
