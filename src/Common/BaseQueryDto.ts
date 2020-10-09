cc3aa768f149a5672132fb0c10901e5277f0b528
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-10-09 21:08:28 +0800

    wip: base query dto

--- src/Common/BaseQueryDto.ts
+++ src/Common/BaseQueryDto.ts
@@ -1,3 +1,28 @@
 export class BaseQueryDto {
-  sort: { key: string };
+  /**排序 */
+  sort: {
+    /**
+     * 排序关键字
+     */
+    key: string;
+
+    /**
+     * 顺序
+     * ASC 升序
+     * DESC 降序
+     */
+    order: 'ASC' | 'DESC';
+  };
+
+  /**分页 */
+  pagination: {
+    /**
+     * 页码 从1开始
+     */
+    page: number;
+    /**
+     * 分页大小
+     */
+    size: number;
+  };
 }
