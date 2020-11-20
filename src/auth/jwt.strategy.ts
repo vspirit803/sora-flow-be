86ac95e452ca1a435814f322b0435de18bd790c9
vspirit803 <52484564+vspirit803@users.noreply.github.com>
2020-11-20 17:12:33 +0800

    feat(auth): return a clearer message when authorization expires

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
@@ -9,12 +9,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: jwtConstants.secret,
     });
   }
 
   async validate(payload: any) {
+    if (payload.exp < Date.now() / 1000) {
+      throw new UnauthorizedException({ statusCode: 401, message: '登陆已失效' });
+    }
     return {
       id: payload.sub,
       name: payload.name,
